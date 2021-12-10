6e9fc79f2363c581b86337e0301f212301d98578
DavitIshkhanyan <davitishkhanyan.01@mail.ru>
2021-12-10 23:19:50 +0400

    grafik skizb

--- main/views.py
+++ main/views.py
@@ -5,6 +5,10 @@ import sched, time
 from bs4 import BeautifulSoup
 import schedule
 import json
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import matplotlib as mpl
 # Create your views here.
 
 def index(request):
@@ -16,8 +20,9 @@ def about(request):
     return render(request, 'main/about.html')
 
 price = 0
+diag = 0
 def priceTracker():
-    global price, data_loaded
+    global price, diag, data_loaded
     url = 'https://finance.yahoo.com/quote/AAPL?p=AAPL'
     response = requests.get(url)
     soup = BeautifulSoup(response.text,'lxml')
@@ -37,22 +42,50 @@ def priceTracker():
     #     data_loaded = json.load(data_file)
     # print(data_loaded)
     # print(price)
+
+    # diag = soup.find_all('div', {'smartphone_Mt(40px)'})
+    diag = soup.find_all('canvas')
+
+    
     return price
 
     # while True:
     #     print('Current Price of Apple: ', priceTracker())
     #     time.sleep(60)
+
+import io
+import base64
+
+
+fig = plt.figure()
+ax = plt.axes()
+
+def fig_to_base64(fig):
+    img = io.BytesIO()
+    fig.savefig(img, format='png',
+                bbox_inches='tight')
+    img.seek(0)
+
+    return base64.b64encode(img.getvalue())
+# encoded = fig_to_base64(fig)
+# encoded = fig_to_base64(1)
+# my_html = '<img src="data:image/png;base64, {}">'.format(encoded.decode('utf-8'))
+
 def refresh(request):
-    global price
+    global price, diag
     # schedule.every(30).seconds.do(priceTracker)
     priceTracker()
+
+    encoded = fig_to_base64(fig)
+    my_html = '<img src="data:image/png;base64, {}">'.format(encoded.decode('utf-8'))
     # while True:
     #     schedule.run_pending()
     #     time.sleep(30)
     # schedule.run_all()
     # context = 
     # print(price)
-    return HttpResponse(price)
+    res = price+my_html
+    return HttpResponse(res)
     # return data_loaded
     return render(request, 'main/index.html', data_loaded)
     # return render(request, 'main/index.html')
